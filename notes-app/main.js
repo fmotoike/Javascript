58ced74003a89d535121661b0cd14eb7c03e34b4
Filipe Motoike <fmotoike@gmail.com>
2019-08-15 12:08:21 -0300

    Adding some features in both apps

--- notes-app/main.js
+++ notes-app/main.js
@@ -1,11 +1,24 @@
-// QUERY SELECTOR 
-// const paragraph = document.querySelector('p');
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Auckland.'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eat better. Meditate.'
+}, {
+    title: 'Study',
+    body: 'CSS. Javascript. React.'
+}];
 
-// paragraph.remove();
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    console.log(e);
+});
 
-// QUERY SELECTOR ALL 
-const paragraphs = document.querySelectorAll('p');
+document.querySelector('#remove-all').addEventListener('click', function (e) {
+    document.querySelectorAll('.note').forEach(function (note) {
+        note.remove();
+    });
+});
 
-paragraphs.forEach(function (p) {
-    p.textContent = 'Yay'
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    console.log(e.target.value);
 })
--- todo-app/main.js
+++ todo-app/main.js
@@ -1,8 +1,38 @@
-const paragraph = document.querySelectorAll('p');
+const todos = [{
+    text: 'Go to the gym',
+    completed: true
+}, {
+    text: 'Study Javascript',
+    completed: true
+}, {
+    text: 'Practice CSS',
+    completed: false
+}, {
+    text: 'Read a book',
+    completed: false
+}, {
+    text: 'Meditate',
+    completed: false
+}];
 
-paragraph.forEach(function (p) {
-    const content = p.textContent;
-    if (content.toLowerCase().includes('the')) {
-        p.remove();
-    }
+const incompleteTodos = todos.filter(function (todo) {
+    return !todo.completed;
 });
+
+const summary = document.createElement('h3');
+summary.textContent = `You have ${incompleteTodos.length} todos left`;
+document.querySelector('body').appendChild(summary);
+
+todos.forEach(function (todo) {
+    let newParagraph = document.createElement('p');
+    newParagraph.textContent = todo.text;
+    document.querySelector('body').appendChild(newParagraph);
+});
+
+document.querySelector('#add-todo').addEventListener('click', function (e) {
+    console.log('I am adding a new to do.');
+})
+
+document.querySelector('#new-todo').addEventListener('input', function (e) {
+    console.log(e.target.value);
+})
