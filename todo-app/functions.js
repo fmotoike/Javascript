5a88f5ea92c909493e9cbd3c3d2ec2d8cf8faefe
Filipe Motoike <fmotoike@gmail.com>
2019-08-18 16:19:13 -0300

    Refactoring both apps

--- todo-app/functions.js
+++ todo-app/functions.js
@@ -1,68 +1,98 @@
-// CHECK FOR SAVED DATA IN LOCAL STORAGE
+// Fetch existing todos from localStorage
 const getSavedTodos = function () {
-    const todosJSON = localStorage.getItem('todos');
-    
+    const todosJSON = localStorage.getItem('todos')
+
     if (todosJSON !== null) {
-        return JSON.parse(todosJSON);
+        return JSON.parse(todosJSON)
     } else {
-        return [];
-    };
-};
-
-// SAVE TODOS
-const saveTodos = function (todos) {
-    localStorage.setItem('todos', JSON.stringify(todos));
-};
-
-// RENDER APPLICATION TODOS
-const renderTodos = function (todos, filters) {
-    const filteredTodos = todos.filter(function (todo) {
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
-        
-        return searchTextMatch && hideCompletedMatch;
-    });
-  
-    const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !todo.completed;
-    });
-    
-    document.querySelector('#todos').innerHTML = '';
-
-    const summary = generateSummaryDOM(incompleteTodos);
-    document.querySelector('#todos').appendChild(summary);
-    
-    filteredTodos.forEach(function (todo) {
-        const todoEl = generateTodoDOM(todo);
-        document.querySelector('#todos').appendChild(todoEl);
-    });
-};
-
-// GENERATE DOM STRUCTURE FOR A TODO
-const generateTodoDOM = function (todo) {
-    const todoEl = document.createElement('div');
-    const todoCheck = document.createElement('input');
-    const todoText = document.createElement('span');
-    const removeButton = document.createElement('button');
-    
-    // SET THE COMPLETED CHECKBOX
-    todoCheck.setAttribute('type', 'checkbox');
-    todoEl.appendChild(todoCheck);
-
-    // SET THE TODO TEXT
-    todoText.textContent = todo.text;
-    todoEl.appendChild(todoText);
-
-    // SET THE REMOVE TODO BUTTON
-    removeButton.textContent = 'x';
-    todoEl.appendChild(removeButton);
-
-    return todoEl;
+        return []
+    }
 }
 
-// GENEREATE DOM STRUCTURE FOR SUMMARY
+// Save todos to localStorage
+const saveTodos = function (todos) {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+// Remove todo by id
+const removeTodo = function (id) {
+    const todoIndex = todos.findIndex(function (todo) {
+        return todo.id === id
+    })
+
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
+}
+
+// Toggle the completed value for a given todo
+const toggleTodo = function (id) {
+    const todo = todos.find(function (todo) {
+        return todo.id === id
+    })
+
+    if (todo !== undefined) {
+        todo.completed = !todo.completed
+    }
+}
+
+// Render application todos based on filters
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+        
+        return searchTextMatch && hideCompletedMatch
+    })
+
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+    document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos))
+
+    filteredTodos.forEach(function (todo) {
+        document.querySelector('#todos').appendChild(generateTodoDOM(todo))
+    })
+}
+
+// Get the DOM elements for an individual note
+const generateTodoDOM = function (todo) {
+    const todoEl = document.createElement('div')
+    const checkbox = document.createElement('input')
+    const todoText = document.createElement('span')
+    const removeButton = document.createElement('button')
+
+    // Setup todo checkbox
+    checkbox.setAttribute('type', 'checkbox')
+    checkbox.checked = todo.completed
+    todoEl.appendChild(checkbox)
+    checkbox.addEventListener('change', function () {
+        toggleTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
+
+    // Setup the todo text
+    todoText.textContent = todo.text
+    todoEl.appendChild(todoText)
+
+    // Setup the remove button
+    removeButton.textContent = 'x'
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', function () {
+        removeTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
+
+    return todoEl
+}
+
+// Get the DOM elements for list summary
 const generateSummaryDOM = function (incompleteTodos) {
-    const summary = document.createElement('h3');
-    summary.textContent = `You have ${incompleteTodos.length} todos left`;
-    return summary;
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    return summary
 }
--- todo-app/main.js
+++ todo-app/main.js
@@ -1,29 +1,30 @@
-const todos = getSavedTodos();
+let todos = getSavedTodos()
 
 const filters = {
     searchText: '',
     hideCompleted: false
-};
+}
 
-renderTodos(todos, filters);
+renderTodos(todos, filters)
 
-document.querySelector('#search').addEventListener('input', function (e) {
-    filters.searchText = e.target.value;
-    renderTodos(todos, filters);
-});
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
 
 document.querySelector('#new-todo').addEventListener('submit', function (e) {
-    e.preventDefault();
+    e.preventDefault()
     todos.push({
-        text: e.target.elements.addTodo.value,
+        id: uuidv4(),
+        text: e.target.elements.text.value,
         completed: false
-    });
-    saveTodos(todos);
-    renderTodos(todos, filters);
-    e.target.elements.addTodo.value = '';
-});
+    })
+    saveTodos(todos)
+    renderTodos(todos, filters)
+    e.target.elements.text.value = ''
+})
 
-document.querySelector('#completed').addEventListener('change', function (e) {
-    filters.hideCompleted = e.target.checked;
-    renderTodos(todos, filters);
-});
+document.querySelector('#hide-completed').addEventListener('change', function (e) {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
+})
