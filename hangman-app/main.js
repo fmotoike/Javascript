cd1a4a8d20df2c40e281c0766977b48d2333403a
Filipe Motoike <fmotoike@gmail.com>
2019-08-20 18:00:35 -0300

    Adjusting some features and structure

--- hangman-app/main.js
+++ hangman-app/main.js
@@ -1,14 +1,14 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 
-const gameOne = new Hangman('Cat', 2)
+const gameOne = new Hangman('New Orleans', 2)
 
-puzzleEl.textContent = gameOne.getPuzzle()
-guessesEl.textContent = gameOne.getStatusMessage()
+puzzleEl.textContent = gameOne.puzzle
+guessesEl.textContent = gameOne.statusMessage
 
 window.addEventListener('keypress', function (e) {
     const guess = String.fromCharCode(e.charCode)
     gameOne.makeGuess(guess)
-    puzzleEl.textContent = gameOne.getPuzzle()
-    guessesEl.textContent = gameOne.getStatusMessage()
+    puzzleEl.textContent = gameOne.puzzle
+    guessesEl.textContent = gameOne.statusMessage
 })
