22d9b04ba7576d61366b6c1721d2cf064668c54d
Filipe Motoike <fmotoike@gmail.com>
2019-08-21 13:04:03 -0300

    Creating HTTP requests and fetching data

--- hangman-app/main.js
+++ hangman-app/main.js
@@ -1,14 +1,30 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 
-const gameOne = new Hangman('New Orleans', 2)
+const gameOne = new Hangman('New Orleans', 4)
 
 puzzleEl.textContent = gameOne.puzzle
 guessesEl.textContent = gameOne.statusMessage
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     gameOne.makeGuess(guess)
     puzzleEl.textContent = gameOne.puzzle
     guessesEl.textContent = gameOne.statusMessage
+})
+
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle)
+    }
+})
+
+getCountry('BR', (error, country) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(`Country name: ${country.name}`)
+    }
 })
